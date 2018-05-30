2d7673f4b4effec58f9c0c4606bbecf64d991c0a
Aleksej Tkachenko <altka@opsway.com>
2018-05-31 02:43:35 +0300

    Added capability get label in a5 and a6 formats

--- src/Api/Labels.php
+++ src/Api/Labels.php
@@ -5,4 +5,14 @@ namespace OpsWay\Shipmile\Api;
 class Labels extends BaseApi
 {
     const API_PATH_POST = 'labels';
+
+    public function createA5(array $data, array $params = [])
+    {
+        return $this->client->post(static::API_PATH_POST . '?label_format=a5_in_a4', $data);
+    }
+
+    public function createA6(array $data, array $params = [])
+    {
+        return $this->client->post(static::API_PATH_POST . '?label_format=a6_in_a4', $data);
+    }
 }
