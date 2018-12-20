248ac46c4b466b429acac727ef34db0d601bd9f7
mobixon <mobixon@gmail.com>
2018-12-20 12:25:04 +0200

    add getAllShipments method for retrieve all shipments list

--- src/Api/Shipments.php
+++ src/Api/Shipments.php
@@ -2,11 +2,19 @@
 
 namespace OpsWay\Shipmile\Api;
 
+/**
+ * Class Shipments
+ * @package OpsWay\Shipmile\Api
+ */
 class Shipments extends BaseApi
 {
     const API_PATH = 'shipments';
     const API_PATH_POST = 'shipments/bulk';
 
+    /**
+     * @param string $shipmentId
+     * @return mixed
+     */
     public function track(string $shipmentId)
     {
         $response = $this->client->get(static::API_PATH . '/', $shipmentId . '/track');
@@ -14,10 +22,26 @@ class Shipments extends BaseApi
         return $response;
     }
 
+    /**
+     * @param array $shipmentIds
+     * @return mixed
+     */
     public function bulkTracking(array $shipmentIds)
     {
         $response = $this->client->post(static::API_PATH . '/tracking/bulk', $shipmentIds);
 
         return $response;
+    }
+
+    /**
+     * https://shipmile.com/docs/api/#get-all-shipments
+     *
+     * @param int $page
+     * @param int $limit
+     * @return mixed
+     */
+    public function getAllShipments(int $page = 1, int $limit = 20)
+    {
+        return $this->client->getList(static::API_PATH, ['page' => $page, 'limit' => $limit > 500 ? 500 : $limit]);
     }
 }
